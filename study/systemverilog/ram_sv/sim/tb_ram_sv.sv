06e0ca3da9a5d5eb20fe1b076014b22876e90861
kms03212 <kms030229@gmail.com>
2026-08-30 14:47:34 +0900

    Add organized RAM SystemVerilog project

---
+++ study/systemverilog/ram_sv/sim/tb_ram_sv.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/08/30 14:46:12
+// Design Name: 
+// Module Name: tb_ram_sv
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_ram_sv(
+
+    );
+endmodule
---
+++ study/systemverilog/ram_sv/src/ram_sv.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/08/30 14:45:47
+// Design Name: 
+// Module Name: ram_sv
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ram_sv(
+
+    );
+endmodule
